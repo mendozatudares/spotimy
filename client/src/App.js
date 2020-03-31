fea5850ccc4e3a12a3df6001fbb996dbbb622998
Kevin Mendoza Tudares <kevinamendoza@users.noreply.github.com>
2020-03-31 00:48:55 -0500

    side menu, playlists, recently played, and songs added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, useCallback} from 'react';
+import React, {Component} from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 
@@ -12,15 +12,20 @@ class App extends Component {
     if (token) {
       spotifyApi.setAccessToken(token);
     }
+    
     this.state = {
       loggedIn: token ? true : false,
-      headerTitle: "Recently Played",
-      viewType: '',
+      header: "Recently Played",
+      view: "",
+      currentAlbum: null,
       currentAritst: null,
-      currentPlaylist: null
+      currentPlaylist: null,
+      currentTop: "4 Weeks",
     };
-    this.updateHeader = this.updateHeader.bind(this);
-    this.updatePlaylist = this.updatePlaylist.bind(this);
+
+    this.handleHeaderChange = this.handleHeaderChange.bind(this);
+    this.handlePlaylistChange = this.handlePlaylistChange.bind(this);
+    this.handleArtistChange = this.handleArtistChange.bind(this);
   }
 
   getHashParams() {
@@ -35,36 +40,51 @@ class App extends Component {
     return hashParams;
   }
 
-  updateHeader(title) {
-    this.setState({ headerTitle: title });
+  handleHeaderChange(title) {
+    this.setState({ 
+      header: title,
+      view: "",
+      currentPlaylist: null
+    });
   }
 
-  updatePlaylist(playlist) {
+  handlePlaylistChange(playlist) {
     this.setState({ 
-      headerTitle: playlist.name,
+      header: playlist.name,
+      view: "playlist",
       currentPlaylist: playlist, 
-      viewType: 'playlist' 
+      
+    });
+  }
+
+  handleArtistChange(artist) {
+    this.setState({
+      header: artist.name,
+      view: "Artist",
+      currentAritst: artist,
     });
   }
 
   render() {
     return (
       <div className="App">
+        
         <div className="app-container">
           { !this.state.loggedIn && <a href='http://localhost:8888'> Login to Spotify </a> }
           { this.state.loggedIn && (
             <div>
               <div className="left-side-section">
-                <SideMenu state={this.state} action={this.updateHeader}/>
-                { this.state.loggedIn && <UserPlaylists state={this.state} action={this.updatePlaylist}/> }
+                <SideMenu header={this.state.header} onHeaderChange={this.handleHeaderChange}/>
+                <UserPlaylists header={this.header} onPlaylistChange={this.handlePlaylistChange}/>
               </div>
               <div className="main-section">
-                <MainHeader state={this.state}/>
+                <MainHeader appState={this.state}/>
                 <div className="main-section-container">
-                  
+                  <MainView appState={this.state}/>
                 </div>
               </div>
-            </div>)}
+            </div>)
+          }
         </div>
       </div>
     );
@@ -73,12 +93,11 @@ class App extends Component {
 
 class SideMenu extends Component {
   renderSideMenu() {
-    const menu = ["Recently Played", "Songs", "Albums", "Artists"];
-
+    const menu = ["Top", "Recently Played", "Songs", "Albums", "Artists"];
     return menu.map(item => {
       return (
-        <li key={item} className={this.props.state.headerTitle === item ? "active side-menu-item" : "side-menu-item"}
-          onClick={() => {this.props.action(item)}}>
+        <li key={item} className={this.props.header === item ? "active side-menu-item" : "side-menu-item"}
+          onClick={() => this.props.onHeaderChange(item)}>
             {item}
         </li> 
       );
@@ -96,8 +115,11 @@ class SideMenu extends Component {
 }
 
 class UserPlaylists extends Component {  
-  state = {
-    userPlaylists: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      userPlaylists: []
+    }
   }
 
   componentDidMount() {
@@ -109,20 +131,18 @@ class UserPlaylists extends Component {
   }
 
   async renderPlaylists() {
-    var headerTitle = this.props.state.headerTitle;
-    var updatePlaylist = this.props.action;
-
+    const header =  this.props.header;
+    const onPlaylistChange = (playlist) => this.props.onPlaylistChange(playlist);
+    
     return spotifyApi.getMe()
       .then(function(user) {
         return spotifyApi.getUserPlaylists(user.id, { limit: 50 });
       })
       .then(function(playlists) {
         return playlists.items.map(playlist => {
-          const getPlaylistSongs = () => {
-            updatePlaylist(playlist);
-          }
           return (
-            <li onClick={getPlaylistSongs} className={headerTitle === playlist.name ? "active side-menu-item" : "side-menu-item"} key={playlist.id}>
+            <li key={playlist.id} className={header === playlist.name ? "active side-menu-item" : "side-menu-item"}
+              onClick={() => onPlaylistChange(playlist)}>
               {playlist.name}
             </li>
           );
@@ -145,21 +165,22 @@ class UserPlaylists extends Component {
 
 class MainHeader extends Component {
   render() {
-    var headerTitle = this.props.state.headerTitle;
-    var viewType = this.props.state.viewType;
-    var currentArtist = this.props.state.currentAritst;
-    var currentPlaylist = this.props.state.currentPlaylist;
+    const header = this.props.appState.header;
+    const view = this.props.appState.view;
+    const currentAlbum = this.props.appState.currentAlbum;
+    const currentArtist = this.props.appState.currentAritst;
+    const currentPlaylist = this.props.appState.currentPlaylist;
 
     return (
       <div className='section-title'>
-        {viewType === "playlist" && (
+        {view === "playlist" && (
           <div className='playlist-title-container'>
             <div className='playlist-image-container'>
-              <img className='playlist-image' src={currentPlaylist.images[0] ? currentPlaylist.images[0].url : null}/>
+              <img className='playlist-image' src={currentPlaylist.images[0] ? currentPlaylist.images[0].url : null} alt={currentPlaylist.name}/>
             </div>
             <div className='playlist-info-container'>
               <p className='playlist-text'>PLAYLIST</p>
-              <h3 className='header-title'>{headerTitle}</h3>
+              <h3 className='header-title'>{header}</h3>
               <p className='created-by'>Created by <span className='lighter-text'>
                 {currentPlaylist.owner.display_name}</span> - {currentPlaylist.tracks.total} songs
               </p>
@@ -167,10 +188,10 @@ class MainHeader extends Component {
           </div>
         )}
 
-        {viewType === "Artist" && currentArtist && (
+        {view === "Artist" && currentArtist && (
           <div>
             <div className='current-artist-header-container'>
-              <img className='current-artist-image' src={currentArtist.images[0].url}/>
+              <img className='current-artist-image' src={currentArtist.images[0].url} alt={currentArtist.name}/>
               <div className='current-artist-info'>
                 <p>Artist from your library</p>
                 <h3>{currentArtist.name}</h3>
@@ -180,12 +201,13 @@ class MainHeader extends Component {
         )}
 
         {(
-          headerTitle === 'Songs'||
-          headerTitle === 'Recently Played' ||
-          headerTitle === 'Albums' ||
-          headerTitle === 'Artists') && (
+          header === 'Top'||
+          header === 'Songs'||
+          header === 'Recently Played' ||
+          header === 'Albums' ||
+          header === 'Artists') && (
             <div>
-              <h3 className='header-title'>{headerTitle}</h3>
+              <h3 className='header-title'>{header}</h3>
             </div>
           )}
       </div>
@@ -193,30 +215,194 @@ class MainHeader extends Component {
   }
 }
 
+class MainView extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.appState.header === "Top" && <TopList/>}
+        {this.props.appState.header === "Artists" && <ArtistList/>}
+        {(
+          this.props.appState.header === "Recently Played" ||
+          this.props.appState.header === "Songs" ||
+          this.props.appState.view === "playlist" )
+          && <SongList appState={this.props.appState}/>}
+      </React.Fragment>
+    );
+  }
+}
+
+class TopList extends Component {
+  render() {
+    return <div></div>
+  }
+}
 
 class AlbumList extends Component {
-  renderAlbum(albumId) {
-    const songs = spotifyApi.getAlbumTracks(albumId);
-    return songs.map((song, i) => {
-      return (
-          <li className="album-item" key={i}>
-              <div>
-                  <div className="album-image">
-                      <img src={song.track.album.images[0].url}/>
-                  </div>
+  render() {
+    return <div></div>
+  }
+}
 
-                  <div className="album-details">
-                      <p className="album-name">{song.track.album.name}</p>
-                      <p className="artist-name">{song.track.album.artists[0].name}</p>
-                  </div>
-              </div>
-          </li>
-      );
-  });
+class ArtistList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userArtists: []
+    }
+  }
+
+  componentDidMount() {
+    this.renderArtists().then(data =>
+      this.setState({
+        userArtists: data
+      })
+    );
+  }
+
+  async renderArtists() {
+    return spotifyApi.getFollowedArtists({ limit: 50 })
+      .then(function(data) {
+        return data.artists.items.map(artist => {
+          const getArtist = () => {
+            //noop
+          }
+          return (
+            <li key={artist.id} className="artist-item"
+              onClick={getArtist}>
+              <a>
+                <div claasName="artist-image">
+                  <img src={artist.images[0] ? artist.images[0].url : ""}/>
+                </div>
+                <div className="artist-details">
+                  <p>{artist.name}</p>
+                </div>
+              </a>
+            </li>
+          );
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
   }
 
   render() {
-    return <ul className="album-view-container">{this.renderAlbum(spotifyApi.getAlbum(this.props.albumId))}</ul>
+    return <div></div>;
+  }
+}
+
+class SongList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: [],
+      visible: true,
+    }
+  }
+
+  componentDidMount() {
+    this.renderSongs().then(data => {
+      this.setState({
+        header: this.props.appState.header,
+        songs: data,
+        visible: true,
+      })
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.state.visible && this.props.appState.header != this.state.header)
+      this.renderSongs().then(data => {
+        this.setState({
+          header: this.props.appState.header,
+          songs: data,
+        })
+      });
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      visible: false,
+    })
+  }
+
+  async renderSongs() {
+    const currentPlaylist = this.props.appState.currentPlaylist;
+    const songList = (items) => this.songList(items);
+
+    switch (this.props.appState.header) {
+      case ("Recently Played"):
+        return spotifyApi.getMyRecentlyPlayedTracks({ limit: 50 })
+          .then(function(data) {
+            return songList(data.items);
+          })
+      case ("Songs"):
+        return spotifyApi.getMySavedTracks({ limit: 50 })
+          .then(function (data) {
+            return songList(data.items);
+          })
+      default: {
+        if (this.props.appState.view === "playlist") {
+          return spotifyApi.getMe()
+            .then(function(user) {
+              return spotifyApi.getPlaylistTracks(user.id, currentPlaylist.id);
+            })
+            .then(function(data) {
+              return songList(data.items);
+            })
+          }
+          
+      }
+    }
+  }
+
+  songList(items) {
+    return items.map((song, i) => {
+      const msToMinutesAndSeconds = (ms) => {
+        const minutes = Math.floor(ms / 60000);
+        const seconds = ((ms % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+      }
+
+      return (
+      <li key={i} className="user-song-item">
+        <div className="song-title">
+          <p>{song.track.name}</p>
+        </div>
+        <div className="song-artist">
+          <p>{song.track.artists[0].name}</p>
+        </div>
+        <div className="song-album">
+          <p>{song.track.album.name}</p>
+        </div>
+        <div className="song-length">
+          <p>{msToMinutesAndSeconds(song.track.duration_ms)}</p>
+        </div>
+      </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="song-header-container">
+          <div className="song-title-header">
+            <p>Title</p>
+          </div>
+          <div className="song-artist-header">
+            <p>Aritst</p>
+          </div>
+          <div className="song-album-header">
+            <p>Album</p>
+          </div>
+          <div className="song-length-header">
+            <p>Length</p>
+          </div>
+        </div>
+        {this.state.songs}
+      </div>
+    );
   }
 }
 
